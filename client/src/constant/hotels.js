4fd18ca204e090d61d9afb212564e09e402e4d80
adnansamol <samoladnan@gmail.com>
2023-04-15 15:24:05 +0530

    Added hotels

--- client/src/constant/hotels.js
+++ client/src/constant/hotels.js
@@ -1,5 +1,25 @@
 const mauritius_hotels = [
   {
+    name: "Le Méridien Ile Maurice",
+    address: "Village Hall Lane, Pointe Aux Piments, Mauritius 21304",
+    rating: 5,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Pool",
+      "Fitness Centre with Gym / Workout Room",
+      "Free breakfast",
+      "Beach",
+      "Spa",
+      "Tennis court",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/23/1d/6c/c2/aerial-view.jpg?w=1000&h=-1&s=1",
+    ],
+    room_types: ["Ocean view", "Suites", "Family rooms"],
+    price_per_room: 4400,
+  },
+  {
     name: "The Westin Turtle Bay Resort & Spa",
     address: "Turtle Bay,Balaclava,Maurititus 21307",
     rating: 4,
@@ -33,8 +53,73 @@ const mauritius_hotels = [
     room_types: ["Ocean view", "Bridal suite", "Suites", "Family rooms"],
     price_per_room: 2400,
   },
+  {
+    name: "Salt Of Palmar",
+    address: "Coastal Road, Palmar, Mauritius 41604",
+    rating: 5,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Fitness Centre with Gym / Workout Room",
+      "Free breakfast",
+      "Beach",
+      "Bar / lounge",
+      "Boating",
+      "Walking tours",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1e/9a/52/29/barlounge.jpg?w=1000&h=-1&s=1",
+    ],
+    room_types: ["Ocean view", "Bridal suite", "Suites", "Family rooms"],
+    price_per_room: 2700,
+  },
+  {
+    name: "Club Med La Plantation d'Albion",
+    address:
+      "Avenue du Club Med, Albion, MauritiusAvenue du Club Med, Albion, Mauritius",
+    rating: 4,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Fitness Centre with Gym / Workout Room",
+      "Free breakfast",
+      "Beach",
+      "Bar / lounge",
+      "Boating",
+      "Walking tours",
+      "Babysitting",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/d4/79/4f/club-med-la-plantation.jpg?w=1000&h=-1&s=1",
+    ],
+    room_types: ["Ocean view", "Bridal suite", "Suites", "Family rooms"],
+    price_per_room: 2700,
+  },
 ];
-const kashmir_hotels = [{}];
+const kashmir_hotels = [
+  {
+    name: "The Lalit Grand Palace Srinagar",
+    address: "Gupkar Road, Srinagar 190001 India",
+    rating: 4,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Pool",
+      "Fitness Centre with Gym / Workout Room",
+      "Free breakfast",
+      "Beach",
+      "Bar / lounge",
+      "Bowling",
+      "Game room",
+      "Kids stay free",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/e7/9b/d0/photo0jpg.jpg?w=1000&h=-1&s=1",
+    ],
+    room_types: ["Bridal Suites", "Suites", "Family Rooms"],
+    price_per_room: 3000,
+  },
+];
 const rajasthan_hotels = [{}];
 const goa_hotels = [
   {
@@ -57,6 +142,7 @@ const goa_hotels = [
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/60/38/00/jw-marriott-goa-vagator.jpg?w=1200&h=-1&s=1",
     ],
     room_types: ["Ocean view", "Suites"],
+    price_per_room: 3000,
   },
   {
     name: "Taj Resort & Convention Centre",
@@ -78,12 +164,13 @@ const goa_hotels = [
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/1d/96/92/taj-hotel-convention.jpg?w=1200&h=-1&s=1",
     ],
     room_types: ["Ocean view", "Suites", "City view"],
+    price_per_room: 3400,
   },
   {
     name: "Zone Connect, Goa, Calangute",
     address:
       "H No 2/16A, Naika Vaddo (near St. Alex Church), Calangute 403516 India",
-    rating: 4.5,
+    rating: 4,
     facilities: [
       "Free parking",
       "Free High Speed Internet (WiFi)",
@@ -100,6 +187,47 @@ const goa_hotels = [
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/23/94/22/66/zone-connect-goa-calangute.jpg?w=1200&h=-1&s=1",
     ],
     room_types: ["Ocean view", "Suites", "Mountain view"],
+    price_per_room: 2400,
+  },
+  {
+    name: "La Grace Resort",
+    address: "Vaswaddo Vaddy Beach, Benaulim 403716 India",
+    rating: 3,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Pool",
+      "Free breakfast",
+      "Beach",
+      "Kids stay free",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/08/4f/d5/la-grace-resort.jpg?w=600&h=-1&s=1",
+    ],
+    room_types: ["Family Rooms"],
+    price_per_room: 2600,
+  },
+  {
+    name: "Bloom Hotel - Calangute",
+    address:
+      "197/37, Calangute - Baga Road Umta Vaddo, Behind McDonalds Restaurant, Calangute 403516 India",
+    rating: 5,
+    facilities: [
+      "Free parking",
+      "Free High Speed Internet (WiFi)",
+      "Pool",
+      "Fitness Centre with Gym / Workout Room",
+      "Free breakfast",
+      "Beach",
+      "24-hour security",
+      "Game room",
+      "Kids stay free",
+    ],
+    images: [
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/e9/2f/b9/bloom-hotel-calangute.jpg?w=1000&h=-1&s=1",
+    ],
+    room_types: ["Family Rooms", "Suites"],
+    price_per_room: 3000,
   },
 ];
 const maldives_hotels = [{}];
